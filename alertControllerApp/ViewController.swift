f95139bd56208702ee2f46a0396e85e07dec3c2f
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-02-19 12:53:28 -0600

    Alert controller initial commit

--- alertControllerApp/ViewController.swift
+++ alertControllerApp/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Warning!", message: "The cheese is melting!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Yum", style: .default, handler: {action -> Void in})
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
